c7d38f367397370fc266a42b1b90ff7445b3bce8
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2021-02-18 21:02:29 +0200

    test: use the new schema design (fructo/fructo#22)

--- tests/schema-compiler/SchemaCompiler.test.ts
+++ tests/schema-compiler/SchemaCompiler.test.ts
@@ -2,70 +2,74 @@
 
 import test from 'ava';
 
-import { docs, inherit, schema, SchemaCompiler } from '@fructo/schema-compiler';
+import { ISchema, SchemaCompiler } from '@fructo/schema-compiler';
 
 
 test('SchemaCompiler.compile() constructs empty TypeScript classes', t => {
     const compiler = new SchemaCompiler();
-    const SCHEMA = {
-        'MyClass': [
-            schema`class`,
-            docs`My Class Description`,
-            {
-            }
-        ]
+    const SCHEMA: ISchema = {
+        MyClass: {
+            types: ['class'],
+            docs: [
+                'My Class Description'
+            ]
+        }
     };
     const compiledCode = compiler.compileSchema(SCHEMA);
     t.deepEqual(compiledCode, [
         '/**',
         ' * My Class Description',
         ' */',
-        'class MyClass {',
+        'export class MyClass {',
         '}'
     ]);
 });
 
 test('SchemaCompiler.compile() constructs empty TypeScript interfaces', t => {
     const compiler = new SchemaCompiler();
-    const SCHEMA = {
-        'MyInterface': [
-            schema`interface`,
-            docs`My Interface Description`,
-            {
-            }
-        ]
+    const SCHEMA: ISchema = {
+        MyInterface: {
+            types: ['interface'],
+            docs: [
+                'My Interface Description'
+            ]
+        }
     };
     const compiledCode = compiler.compileSchema(SCHEMA);
     t.deepEqual(compiledCode, [
         '/**',
         ' * My Interface Description',
         ' */',
-        'interface MyInterface {',
+        'export interface MyInterface {',
         '}'
     ]);
 });
 
 test('SchemaCompiler.compile() constructs an interface property', t => {
     const compiler = new SchemaCompiler();
-    const SCHEMA = {
-        'MyInterface': [
-            schema`interface`,
-            docs`My Interface Description`,
-            {
-                'myProperty': [
-                    docs`My Property Description`,
-                    [['string'], ['number']],
-                    ['readonly', 'optional']
-                ]
+    const SCHEMA: ISchema = {
+        MyInterface: {
+            types: ['interface'],
+            docs: [
+                'My Interface Description'
+            ],
+            properties: {
+                myProperty: {
+                    types: [['string'], ['number']],
+                    keywords: ['optional'],
+                    docs: [
+                        'My Property Description'
+                    ]
+                }
             }
-        ]
+        }
     };
     const compiledCode = compiler.compileSchema(SCHEMA);
     t.deepEqual(compiledCode, [
         '/**',
         ' * My Interface Description',
         ' */',
-        'interface MyInterface {',
+        'export interface MyInterface {',
         '    /**',
         '     * My Property Description',
         '     */',
@@ -76,20 +80,20 @@ test('SchemaCompiler.compile() constructs an interface property', t => {
 
 test('SchemaCompiler.compile() constructs interface inheritance', t => {
     const compiler = new SchemaCompiler();
-    const SCHEMA = {
-        'MyAncestorInterface': [
-            schema`interface`
-        ],
-        'MyInterface': [
-            schema`interface`,
-            inherit`MyAncestorInterface`
-        ]
+    const SCHEMA: ISchema = {
+        MyAncestorInterface: {
+            types: ['interface']
+        },
+        MyInterface: {
+            types: ['interface'],
+            inherits: ['MyAncestorInterface']
+        }
     };
     const compiledCode = compiler.compileSchema(SCHEMA);
     t.deepEqual(compiledCode, [
-        `interface MyAncestorInterface {`,
+        `export interface MyAncestorInterface {`,
         '}',
-        'interface MyInterface extends MyAncestorInterface {',
+        'export interface MyInterface extends MyAncestorInterface {',
         '}'
     ]);
 });
