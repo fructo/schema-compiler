ac4fcc95f853b7e2fe54eebf1a2265c9aa542a96
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2021-03-10 08:49:34 +0200

    test: add a dummy test

--- tests/schema-compiler-core/SchemaCompilerCore.test.ts
+++ tests/schema-compiler-core/SchemaCompilerCore.test.ts
@@ -2,6 +2,50 @@
 
 import test from 'ava';
 
+import { SchemaCompilerCore } from '@fructo/schema-compiler';
+
+
 test('dummy', t => {
+    new SchemaCompilerCore().compileSchema({
+        "TString": {
+            "description": "is_string",
+            "rule": "typeof value === 'string'",
+            "type": "string"
+        },
+        "TNumber": {
+            "description": "is_number",
+            "rule": "typeof value === 'number'",
+            "type": "number"
+        },
+        "ISettings": {
+            "properties": {
+                "host": "TString | '127.0.0.1'",
+                "port": "TNumber | 42",
+                "uf": {
+                    "o": "TString | 10",
+                }
+            }
+        },
+        "IMessage": {
+            "properties": {
+                "header": "(TString | TString) & ISettings & TString"
+            }
+        },
+        "IMessageSettingsLoad": {
+            "ancestors": "IMessage",
+            "properties": {
+                "header": "'message-settings-load'",
+                "settings": "ISettings"
+            }
+        },
+        "MessageFromDaemon": [
+            "IMessageSettingsLoad"
+        ],
+        "Ok": {
+            "properties": {
+                "MI": "TString"
+            }
+        }
+    });
     t.pass();
 });
