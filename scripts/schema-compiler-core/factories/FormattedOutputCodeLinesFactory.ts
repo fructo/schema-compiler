9a4de473876da7473d37b2d96cb3ced0a6fa93dd
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2021-03-12 19:57:31 +0200

    fix: calculate indents properly

--- scripts/schema-compiler-core/factories/FormattedOutputCodeLinesFactory.ts
+++ scripts/schema-compiler-core/factories/FormattedOutputCodeLinesFactory.ts
@@ -18,16 +18,15 @@ export class FormattedOutputCodeLinesFactory {
         let bracketsCount = 0;
         for (let i = 0; i < lines.length; i++) {
             const line = lines[i];
-            const lineBracketsCount = (line.split('{').length - 1) - (line.split('}').length - 1);
-            if (lineBracketsCount < 0) {
+            const noSpacesLine = line.slice(line.split('').findIndex(char => char !== ' '));
+            if (/^[})]/.test(noSpacesLine)) {
                 bracketsCount--;
             }
-            const actualCount = Math.max(0, line.split('').findIndex(char => char !== ' '));
-            const requiredCount = Math.max(0, 4 * bracketsCount - (actualCount - actualCount % 2));
-            if (lineBracketsCount > 0) {
+            const spacesCount = 4 * bracketsCount;
+            lines[i] = `${' '.repeat(spacesCount)}${noSpacesLine}`;
+            if (/[{(]$/.test(line)) {
                 bracketsCount++;
             }
-            lines[i] = `${' '.repeat(requiredCount)}${line}`;
         }
     }
 
